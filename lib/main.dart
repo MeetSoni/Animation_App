f8aa96e26ceef7bbd50bce07af671ee8196ab23b
MeetSoni <64322534+MeetSoni@users.noreply.github.com>
2020-09-27 11:19:01 +0530

    Add files via upload
    lib commited

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:Animation_app/animation.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Animation App'),
+      ),
+      body: MyAni(),
+    ),
+  ));
+}
